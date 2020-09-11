5b48bd23388a70684a0532aad9e2e63767c11206
FrenchieDev <68165676+FrenchieDev@users.noreply.github.com>
2020-09-11 16:25:59 -0400

    Add files via upload

---
+++ build/precache-manifest.b061829358466e1406c0af50b3db3731.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698d1d01654552f1053490ca90636e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c6b60238949dee266a7",
+    "url": "/static/css/main.15128f70.chunk.css"
+  },
+  {
+    "revision": "2266be0c0c20108d066f",
+    "url": "/static/js/2.97701fec.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.97701fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6b60238949dee266a7",
+    "url": "/static/js/main.55e18a18.chunk.js"
+  },
+  {
+    "revision": "2e075117383e7b156060",
+    "url": "/static/js/runtime-main.2f0bb968.js"
+  },
+  {
+    "revision": "dd6f7569bc7c03b69fba39f75eb6bc99",
+    "url": "/static/media/WorldMapBackground.dd6f7569.jpg"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
